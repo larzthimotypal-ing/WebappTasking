d0377e3aeba4f590600a016fbbd0534f33814e9d
thimotypaling <thimotypaling@gmail.com>
2020-11-19 17:02:53 +0800

    Add project files.

---
+++ WebappTasking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebappTasking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
